472a31e0cf5b1cc28bea7125adafd07588ccefa9
praful <praful.khuman@ics-global.in>
2023-03-31 13:24:54 +0530

    Commit

--- src/components/report/SaleReport.js
+++ src/components/report/SaleReport.js
@@ -5,7 +5,7 @@ function SaleReport() {
 		let originalContents = document.body.innerHTML;
 		document.body.innerHTML = printContents;
 		window.print();
-		document.body.innerHTML = originalContents; 
+		document.body.innerHTML  = originalContents; 
    
 	};
 	return (
